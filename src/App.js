d617515baa10e3674c9bedccc3e042ca9779f10b
Sofia Atsalou <sofia.atsalou@gmail.com>
2020-04-26 19:41:01 +0300

    feat(App) Place Burger Builder component under Layout component

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import React, { Component } from 'react';
 import './App.css';
 
 import Layout from './components/Layout/Layout';
+import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Layout>
-          <p>Test</p>
+          <BurgerBuilder />
         </Layout>
       </div>
     );
