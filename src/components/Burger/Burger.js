778e83abfb690f76f231bfc04ec477be2a40c815
Sofia Atsalou <sofia.atsalou@gmail.com>
2020-04-26 20:54:20 +0300

    feat(Burger) Create Burger functional component with its own CSS module

---
+++ src/components/Burger/Burger.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import classes from './Burger.module.css';
+
+const burger = (props) => {
+    return (
+        <div className={classes.Burger}>
+            
+        </div>
+    );
+}
+
+export default burger;
