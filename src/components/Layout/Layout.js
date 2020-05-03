35226032d537375e7fc0e5be8eec19d71595da03
Sofia Atsalou <sofia.atsalou@gmail.com>
2020-05-03 23:38:47 +0300

    feat(SideDrawer) Show backdrop only when the sidedrawer is open and hide sidedrawer when backdrop is clicked

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,19 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import Aux from '../../hoc/Aux';
 import classes from './Layout.module.css';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
 import SideDrawyer from '../Navigation/SideDrawer/SideDrawer';
 
-const layout = (props) => (
-    <Aux>
-        <Toolbar />
-        <SideDrawyer />
-        <div>Toolbar, SideDrawyer, Backdrop</div>
-        <main className={classes.Content}>
-            {props.children}
-        </main>
-    </Aux>
-);
+class Layout extends Component {
+    state = {
+        showSideDrawer: true
+    }
 
-export default layout;
+    sideDrawyerClosedHandler = () => {
+        this.setState({showSideDrawer: false});
+    }
+
+    render() {
+        return (
+            <Aux>
+                <Toolbar />
+                <SideDrawyer
+                    open={this.state.showSideDrawer}
+                    closed={this.sideDrawyerClosedHandler} />
+                <div>Toolbar, SideDrawyer, Backdrop</div>
+                <main className={classes.Content}>
+                    {this.props.children}
+                </main>
+            </Aux>
+        );
+    }
+}
+
+export default Layout;
