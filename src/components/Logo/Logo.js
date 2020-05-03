8de5b2a086d8290117b0ba1322ce9361e285fe91
Sofia Atsalou <sofia.atsalou@gmail.com>
2020-05-03 22:11:29 +0300

    feat(Logo) Set height dynamically through props (is not used/just for demo)

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -6,7 +6,7 @@ import burgerLogo from '../../assets/images/burger-logo.png';
 
 const logo = (props) => {
     return (
-        <div className={classes.Logo}>
+        <div className={classes.Logo} style={{heihgt: props.heihgt}}>
             <img src={burgerLogo} alt="My Burger" />
         </div>
     );
