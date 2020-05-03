882fc248870919f1e281234f5078b0c1c7b2fd27
Sofia Atsalou <sofia.atsalou@gmail.com>
2020-05-03 22:03:24 +0300

    feat(SideDrawer) Create SideDrawer component and place it under Layout

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -3,10 +3,12 @@ import React from 'react';
 import Aux from '../../hoc/Aux';
 import classes from './Layout.module.css';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
+import SideDrawyer from '../Navigation/SideDrawer/SideDrawer';
 
 const layout = (props) => (
     <Aux>
         <Toolbar />
+        <SideDrawyer />
         <div>Toolbar, SideDrawyer, Backdrop</div>
         <main className={classes.Content}>
             {props.children}
